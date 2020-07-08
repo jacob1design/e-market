50e3e2ead53dbe57b8c81293b029a503357d2dbd
jacob <mercihabam@gmail.com>
2020-07-08 20:15:36 +0200

    add requirements

--- e_market/settings.py
+++ e_market/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'b*$=h6)rztda!cd*zw$()n@*j+0p9(gwf-vo@)v1ir38_x2*ax'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mercijaco1.pythonanywhere.com'
+]
 
 
 # Application definition
